a4e2903aee2c18e700b896ef1c0f38fa528b9fe3
Prianshu Mitra <prianshumitraprivateserver1@gmail.com>
2026-08-21 12:46:01 +0530

    Refactor oddEvenList for efficiency
    Refactor oddEvenList method to eliminate unnecessary list and simplify logic.

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -1,49 +1,20 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    boolean isEven(int x) {
-        return (x%2 == 0);
-    }
-
     public ListNode oddEvenList(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        ListNode ptr = head;
-        ListNode preptr = null;
-        int i = 1;
         
-        if(head == null) {
-            return null;
-        }
+        if(head == null || head.next == null) return head;
 
-        while(ptr != null) {
-            if(isEven(i)) {
-                list.add(ptr.val);
-                preptr.next = ptr.next;
-                ptr = ptr.next;
-            }
-            else {
-                preptr = ptr;
-                ptr = ptr.next;
-            }
-            i++;
-        }
-        ptr = head;
-        while(ptr.next != null) ptr = ptr.next;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
 
-        for(int x : list) {
-            ListNode new_node = new ListNode(x);
-            ptr.next = new_node;
-            ptr = ptr.next;
-        }
+        while(even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
 
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
         return head;
     }
 }
