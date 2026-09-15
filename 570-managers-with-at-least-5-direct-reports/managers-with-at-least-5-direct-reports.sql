7905ed8e7b4b70d4938b7fd7470a626c1027caf8
Prianshu Mitra <prianshumitraprivateserver1@gmail.com>
2026-09-15 10:40:19 +0530

    Time: 350 ms (25.42%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+select name from Employee
+where id in (select managerId from Employee
+where managerId is not null group by managerId
+having count(*) >= 5);
